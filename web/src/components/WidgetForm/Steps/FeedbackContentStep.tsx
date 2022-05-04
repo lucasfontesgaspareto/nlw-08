d6e68fec86e254f81148e9c93db3c86dbabb34a3
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-05-04 20:59:51 -0300

    [web] update: FeedbackContentStep

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
@@ -13,6 +13,11 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
   const feedbackTypeInfo = feedbackTypes[props.feedbackType]
   
   const [screenshot, setScreenshot] = useState<string | null>(null)
+  const [comment, setComment] = useState('')
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault()
+  }
 
   return <>
     <header>
@@ -32,8 +37,9 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
       <CloseButton/>
     </header>
 
-    <form className="my-4 w-full">
+    <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
       <textarea
+        onChange={event => setComment(event.target.value)}
         className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 p-2 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
         placeholder="Conte com detalhes o que está acontecendo..."
       ></textarea>
@@ -45,8 +51,9 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
         />
 
         <button
+          disabled={!comment && !screenshot}
           type="submit"
-          className="p-2 bg-brand-500 hover:bg-brand-300 rounded-md border-transparent flex-1 justify-center items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          className="p-2 bg-brand-500 hover:bg-brand-300 rounded-md border-transparent flex-1 justify-center items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
         >Enviar feedback</button>
       </footer>
     </form>
