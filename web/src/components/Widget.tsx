7c908555dcdb3a6fc45211780c4975537b022a2a
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-05-03 22:15:27 -0300

    [web] add headless ui

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,20 +1,13 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 interface WidgetProps {}
 
 export function Widget(props: WidgetProps) {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+  return <Popover className="absolute bottom-4 right-4">
+    <Popover.Panel>Hello World</Popover.Panel>
 
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
-
-  return <div className="absolute bottom-4 right-4">
-    {isWidgetOpen && <p>Hello World</p>}
-
-    <button
-      onClick={toggleWidgetVisibility}
+    <Popover.Button
       className="bg-brand-500 text-white rounded-full px-3 h-12 flex items-center group"
     >
       <ChatTeardropDots className="w-6 h-6" />
@@ -22,6 +15,6 @@ export function Widget(props: WidgetProps) {
       <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
         Feedback
       </span>
-    </button>
-  </div>
+    </Popover.Button>
+  </Popover>
 }
