28c92c730faad04d1c5d3680c762f2a9afcd3bb0
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-05-03 22:10:41 -0300

    [web] update Widget/add phosphor-react

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,3 +1,27 @@
-export function Widget() {
-  return <div>widget</div>
+import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
+
+interface WidgetProps {}
+
+export function Widget(props: WidgetProps) {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
+
+  return <div className="absolute bottom-4 right-4">
+    {isWidgetOpen && <p>Hello World</p>}
+
+    <button
+      onClick={toggleWidgetVisibility}
+      className="bg-brand-500 text-white rounded-full px-3 h-12 flex items-center group"
+    >
+      <ChatTeardropDots className="w-6 h-6" />
+
+      <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+        Feedback
+      </span>
+    </button>
+  </div>
 }
