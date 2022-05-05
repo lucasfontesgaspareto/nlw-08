dd7a95eb8ff83e4319c847be53d73e431daf1bb7
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-05-04 22:21:23 -0300

    [server] create feedback collection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,26 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  res.send('Hello World!!')
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+  const {
+    type,
+    comment,
+    screenshot,
+  } = req.body
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return res.status(201).json({ data: feedback })
 })
 
 const PORT = process.env.PORT || 3333
