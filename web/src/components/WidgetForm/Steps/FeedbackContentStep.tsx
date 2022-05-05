ad7bf12b8ac052102164e6e69f09cacfb8b8b1f4
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-05-04 22:00:23 -0300

    [web] update FeedbackContentStep

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -57,7 +57,7 @@ export function FeedbackContentStep({
         />
 
         <button
-          disabled={!comment && !screenshot}
+          disabled={!comment}
           type="submit"
           className="p-2 bg-brand-500 hover:bg-brand-300 rounded-md border-transparent flex-1 justify-center items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
         >Enviar feedback</button>
